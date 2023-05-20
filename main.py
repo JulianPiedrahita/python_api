cce6c02ef2eed56d5e15dcb7af6ba01a61791b70
julian piedrahita <julianpiedrahita@MacBook-Pro-de-julian.local>
2023-05-20 13:20:41 -0500

    creacion de templetes

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+data_user = ['bloque', 'apartamento', 'nom_propietario']
 @app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+@app.route('/hello')
 def hello():
-    return 'hello world flask'
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'data_user' : data_user,
+    }
+
+    return render_template('hello.html', **context)
